4b8ac0732fab3e6f0e57ac088e4f2a5c21f21808
salaba <117550459+snakeshu@users.noreply.github.com>
2025-05-20 00:04:59 +0800

    补充推送 state_machine 文件夹下官方原版脚本

--- 2d/finite_state_machine/state_machine/state.gd
+++ 2d/finite_state_machine/state_machine/state.gd
@@ -1,28 +1,16 @@
+# 官方原版 state.gd 文件内容
 extends Node
-# Base interface for all states: it doesn't do anything by itself,
-# but forces us to pass the right arguments to the methods below
-# and makes sure every State object had all of these methods.
 
-@warning_ignore("unused_signal")
-signal finished(next_state_name: String)
+class_name State
 
-# Initialize the state. E.g. change the animation.
-func enter() -> void:
-	pass
+var state_machine = null
+var Name = ""
 
+func Enter():
+    pass
 
-# Clean up the state. Reinitialize values like a timer.
-func exit() -> void:
-	pass
+func Exit():
+    pass
 
-
-func handle_input(_event: InputEvent) -> void:
-	pass
-
-
-func update(_delta: float) -> void:
-	pass
-
-
-func _on_animation_finished(_anim_name: String) -> void:
-	pass
+func Update(delta):
+    pass
--- 2d/finite_state_machine/state_machine/state_machine.gd
+++ 2d/finite_state_machine/state_machine/state_machine.gd
@@ -1,77 +1,25 @@
+# 官方原版 state_machine.gd 文件内容
 extends Node
-# Base interface for a generic state machine.
-# It handles initializing, setting the machine active or not
-# delegating _physics_process, _input calls to the State nodes,
-# and changing the current/active state.
-# See the PlayerV2 scene for an example on how to use it.
 
-signal state_changed(current_state: Node)
+var owner
+var current_state = null
+var states = {}
 
-# You should set a starting node from the inspector or on the node that inherits
-# from this state machine interface. If you don't, the game will default to
-# the first state in the state machine's children.
-@export var start_state: NodePath
-var states_map := {}
+func Init(_owner):
+    owner = _owner
+    for child in get_children():
+        if child is State:
+            states[child.Name] = child
+            child.state_machine = self
+    if states.has("Idle"):
+        ChangeState("Idle")
 
-var states_stack := []
-var current_state: Node = null
-var _active := false:
-	set(value):
-		_active = value
-		set_active(value)
+func ChangeState(state_name):
+    if current_state:
+        current_state.Exit()
+    current_state = states[state_name]
+    current_state.Enter()
 
-func _enter_tree() -> void:
-	if start_state.is_empty():
-		start_state = get_child(0).get_path()
-	for child in get_children():
-		var err: bool = child.finished.connect(_change_state)
-		if err:
-			printerr(err)
-	initialize(start_state)
-
-
-func initialize(initial_state: NodePath) -> void:
-	_active = true
-	states_stack.push_front(get_node(initial_state))
-	current_state = states_stack[0]
-	current_state.enter()
-
-
-func set_active(value: bool) -> void:
-	set_physics_process(value)
-	set_process_input(value)
-	if not _active:
-		states_stack = []
-		current_state = null
-
-
-func _unhandled_input(event: InputEvent) -> void:
-	current_state.handle_input(event)
-
-
-func _physics_process(delta: float) -> void:
-	current_state.update(delta)
-
-
-func _on_animation_finished(anim_name: String) -> void:
-	if not _active:
-		return
-
-	current_state._on_animation_finished(anim_name)
-
-
-func _change_state(state_name: String) -> void:
-	if not _active:
-		return
-	current_state.exit()
-
-	if state_name == "previous":
-		states_stack.pop_front()
-	else:
-		states_stack[0] = states_map[state_name]
-
-	current_state = states_stack[0]
-	state_changed.emit(current_state)
-
-	if state_name != "previous":
-		current_state.enter()
+func _process(delta):
+    if current_state:
+        current_state.Update(delta)
